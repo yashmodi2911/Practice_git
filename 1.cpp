1f5e0eb97edb185f5341d2afd4819c5796e2305f
yashmodi2911 <yashmodi817@gmail.com>
2022-01-11 20:43:22 +0530

    first commit

---
+++ 1.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+class Solution{
+    public:
+      void solve()
+      {
+        int r,b,d;
+        cin>>r>>b>>d;
+        int total=min(r,b);
+        r-=total;
+        b-=total;
+        int find=max(r,b);
+        if(d==0)
+        {
+            if(r==b)
+            cout<<"YES";
+            else
+            cout<<"NO";
+            return;
+        }
+        int fin=total*d;
+        if(fin<find)
+        cout<<"NO";
+        else
+        cout<<"YES";
+
+        
+        
+ 
+      }
+};
+
+int32_t main(){
+    int t;
+    cin>>t;
+    Solution obj;
+    while(t--)
+    {obj.solve();
+     cout<<endl;
+    }
+}
